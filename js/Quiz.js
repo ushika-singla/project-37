64e055b64fa5eb2b3f8621abf5285454c8c58836
ushika-singla <72975568+ushika-singla@users.noreply.github.com>
2021-05-08 16:50:46 +0530

    Add files via upload

--- js/Quiz.js
+++ js/Quiz.js
@@ -31,4 +31,31 @@ async start(){
         question.display();
       }
     }
+
+play(){
+
+   question.hide();
+   background("yellow");
+   textSize(30);
+   text("Result of the Quiz",400,50);
+   Contestant.getContestantInfo();
+   console.log(allContestants);
+   if(allContestants!=undefined){
+    var y=300;
+      for(var plr in allContestants){
+         var correctAns = "2";
+         if(correctAns === allContestants[plr].answer){
+            fill("green");
+         }
+         else
+            fill("red");
+
+            text(allContestants[plr].name + " : " + allContestants[plr].answer,100,y);
+            y=y+30;
+      }
+
+   }
+
+}
+
 }
